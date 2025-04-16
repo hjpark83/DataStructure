c3a32a7b6c811b9b4185eaf8966cb1982a84a7a2
Hyunjoon83 <junippini83@hanyang.ac.kr>
2025-04-16 10:45:27 +0900

    DataStructure

--- 5주차 (Binary Search Tree)/BST.c
+++ 5주차 (Binary Search Tree)/BST.c
@@ -9,6 +9,7 @@ typedef struct Node{
 
 void initTreeNode(Node** pNode, int value);
 void insertTreeNode(Node** p, int value);
+void deleteTreeNode(Node** p, int value);
 void printTreePreorder(Node* pNode);
 void printTreeInorder(Node* pNode);
 void printTreePostorder(Node* pNode);
@@ -24,13 +25,47 @@ int main(){
     insertTreeNode(&pParentNode, 5);
     insertTreeNode(&pParentNode, 7);
 
-    printf("Preorder : \n");
+    printf("After Inserting:");
+    printf("\n  Preorder : ");
     printTreePreorder(pParentNode);
-    printf("\nInorder : \n");
+    printf("\n  Inorder : ");
     printTreeInorder(pParentNode);
-    printf("\nPostorder : \n");
+    printf("\n  Postorder : ");
     printTreePostorder(pParentNode);
     
+    printf("\n-----------------------------------");
+    
+    deleteTreeNode(&pParentNode, 4); // 루트 노드 삭제
+    printf("\nAfter deleting 4:");
+    
+    printf("\n  Preorder : ");
+    printTreePreorder(pParentNode);
+    printf("\n  Inorder : ");
+    printTreeInorder(pParentNode);
+    printf("\n  Postorder : ");
+    printTreePostorder(pParentNode);
+    
+    printf("\n");
+
+    deleteTreeNode(&pParentNode, 1); // 자식 노드가 없는 경우
+    printf("\nAfter deleting 1:");
+    printf("\n  Preorder : ");
+    printTreePreorder(pParentNode);
+    printf("\n  Inorder : ");
+    printTreeInorder(pParentNode);
+    printf("\n  Postorder : ");
+    printTreePostorder(pParentNode);
+    printf("\n");
+
+    deleteTreeNode(&pParentNode, 6); // 자식 노드가 하나인 경우
+    
+    printf("\nAfter deleting 6:");    
+    printf("\n  Preorder : ");
+    printTreePreorder(pParentNode);
+    printf("\n  Inorder : ");
+    printTreeInorder(pParentNode);
+    printf("\n  Postorder : ");
+    printTreePostorder(pParentNode);
     printf("\n");
     return 0;
 }
@@ -49,6 +84,40 @@ void insertTreeNode(Node** p, int value){
         insertTreeNode(&((*p)->leftChild), value);
     }else{
         insertTreeNode(&((*p)->rightChild), value);
+    }
+}
+
+void deleteTreeNode(Node** p, int value){
+    if((*p) == NULL){
+        return;
+    }
+
+    if((*p)->data > value){
+        deleteTreeNode(&((*p)->leftChild), value);
+    }else if((*p)->data < value){
+        deleteTreeNode(&((*p)->rightChild), value);
+    }else{
+        Node *tmp = NULL;
+    
+        if((*p)->leftChild == NULL && (*p)->rightChild == NULL){
+            free(*p);
+            *p = NULL;
+        }else if((*p)->leftChild == NULL){
+            tmp = *p;
+            *p = (*p)->rightChild;
+            free(tmp);
+        }else if((*p)->rightChild == NULL){
+            tmp = *p;
+            *p = (*p)->leftChild;
+            free(tmp);
+        }else{
+            Node* minNode = (*p)->rightChild;
+            while(minNode->leftChild != NULL){
+                minNode = minNode->leftChild;
+            }
+            (*p)->data = minNode->data;
+            deleteTreeNode(&((*p)->rightChild), minNode->data);
+        }
     }
 }
 
